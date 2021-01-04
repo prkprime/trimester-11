c49a2b9fddefffb2e8ea5d3576af890c8b5a93b0
Pratik Gorade <pratikgorade0@gmail.com>
2021-01-05 00:30:04 +0530

    DFCL: Assignment 6: program to calculate and check the hash of the files
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

---
+++ DFCL/assignment-6/hashpy.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import os
+from hashlib import sha3_512
+from tabulate import tabulate
+
+# instead of using memory equals to the file size,
+# we will read 64kb chunk at a time and calculate the hash
+BUF_SIZE = 65536 # 64kb
+
+sha3 = sha3_512()
+
+def calc_file_hash(filename: str) -> str:
+    '''
+    param filename : filename of the file that you wan't to check the hash of
+    returns : hash value of that file. exits the program if file not found
+    '''
+    try:
+        with open(filename, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE) # reading next 64kb of file
+                if not data:
+                    break
+                sha3.update(data)
+        return sha3.hexdigest()
+    except FileNotFoundError:
+        sys.exit(f'{sys.argv[1]} : file not found. exiting...\n')
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == '--check':
+        try:
+            with open('sha3sum', 'r') as f:
+                data = json.load(f)
+                results = []
+                for filename, hash_value in data.items():
+                    if os.path.exists(filename):
+                        if hash_value == calc_file_hash(filename):
+                            results.append([filename, 'OK'])
+                        else:
+                            results.append([filename, 'ERROR'])
+                    else:
+                        results.append([filename, 'FILE_NOT_FOUND'])
+                print(
+                    tabulate(
+                        results,
+                        headers=(
+                            'FileName',
+                            'Status'
+                        )
+                    )
+                )
+                
+        except FileNotFoundError:
+            sys.exit(f'sha3sum : file not found. exiting...\n')
+    else:
+        filename = sys.argv[1]
+        hash_value = calc_file_hash(filename)
+        print(f'{filename} : {hash_value}')
+        try:
+            with open('sha3sum', "r+") as f:
+                data = json.load(f)
+                data.update({filename : hash_value})
+                f.seek(0)
+                json.dump(data, f)
+        except FileNotFoundError:
+            with open('sha3sum', 'w') as f:
+                json.dump({filename : hash_value}, f)
+        
+else:
+    sys.exit(f'Syntax error\n\tUse \'hashpy <filename>\' to calculate the hash.\n\tUse \'hashpy --check\' to check')
